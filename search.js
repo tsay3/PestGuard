280d42e60a93f8d7f72da3843d3ffd9f0e3c63be
IsaacS2Hud <79726528+IsaacS2@users.noreply.github.com>
2023-11-09 20:47:33 -0600

    Finished the search page

--- search.js
+++ search.js
@@ -1,12 +1,17 @@
 
 // for moving from search page to info pages
 
-$(document).on('click', ".listWeed", function (evt) {
+$(document).on('click', ".pest", function (evt) {
     /* TODO: Take the user to the pest info page */
     console.log("on to the pest page");
 });
 
-$(document).on('click', ".listPest", function (evt) {
-    /* TODO: Take the user to the pest info page */
-    console.log("on to the pest page");
-});
+// $(document).on('click', ".listWeed", function (evt) {
+//     /* Take the user to the pest info page */
+//     console.log("on to the pest page");
+// });
+
+// $(document).on('click', ".listPest", function (evt) {
+//     /* Take the user to the pest info page */
+//     console.log("on to the pest page");
+// });
--- searchEntry.js
+++ searchEntry.js
@@ -1,5 +1,4 @@
 
-
 class SearchEntry {
     constructor(
         name,
