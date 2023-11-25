45b2bdbff411ddbcf06524354d06701101e7cd01
IsaacS2Hud <79726528+IsaacS2@users.noreply.github.com>
2023-11-25 17:31:51 -0600

    Added updated plant info page files

--- plant-info-script.js
+++ plant-info-script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const pestsButton = document.getElementById('pests-button');
     const weedsButton = document.getElementById('weeds-button');
     const commonPestList = document.querySelector('.info-box');
@@ -22,99 +22,99 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     ];
 
-    const weedsData = [
-        {
-            name: "Wild Mustard",
-            image: "plant-info-images/wild-mustard.jpeg",
-            destination: "pest_results.html",
+const weedsData = [
+    {
+        name: "Wild Mustard",
+        image: "plant-info-images/wild-mustard.jpeg",
+        destination: "pest_results.html",
 
-        },
-        {
-            name: "Barnyard Grass",
-            image: "plant-info-images/barnyard-grass.jpg",
-            destination: "pest_results.html",
-        },
-        {
-            name: "Field Bindweed",
-            image: "plant-info-images/field-bindweed.jpg",
-            destination: "pest_results.html",
+    },
+    {
+        name: "Barnyard Grass",
+        image: "plant-info-images/barnyard-grass.jpg",
+        destination: "pest_results.html",
+    },
+    {
+        name: "Field Bindweed",
+        image: "plant-info-images/field-bindweed.jpg",
+        destination: "pest_results.html",
 
-        },
+    },
 
-    ];
+];
 
-    pestsButton.addEventListener('click', function () {
-        showPests(pestsData);
+pestsButton.addEventListener('click', function() {
+    showPests(pestsData);
 
-        pestsButton.style.backgroundColor = '#4CAF50';
-        pestsButton.style.color = '#fff';
+    pestsButton.style.backgroundColor = '#4CAF50';
+    pestsButton.style.color = '#fff';
 
-        weedsButton.style.backgroundColor = '#fff';
-        weedsButton.style.color = '#000';
+    weedsButton.style.backgroundColor = '#fff';
+    weedsButton.style.color = '#000';
 
+});
+
+weedsButton.addEventListener('click', function() {
+    showWeeds(weedsData);
+
+    weedsButton.style.backgroundColor = '#4CAF50';
+    weedsButton.style.color = '#fff';
+
+    pestsButton.style.backgroundColor = '#fff';
+    pestsButton.style.color = '#000';
+
+
+});
+
+function showPests(pests) {
+    commonPestList.innerHTML = ''; // Clear the existing content
+
+    pests.forEach(pest => {
+        const pestItem = document.createElement('div');
+        pestItem.className = 'pest';
+
+        const pestLink = document.createElement('a');  // anchor element
+        pestLink.href = pest.destination;
+
+        const pestImage = document.createElement('img');
+        pestImage.src = pest.image;
+        pestImage.className = 'listIcon';
+
+        const pestName = document.createElement('p');
+        pestName.textContent = pest.name;
+
+        pestLink.appendChild(pestImage);
+        pestLink.appendChild(pestName);
+
+        pestItem.appendChild(pestLink);
+        commonPestList.appendChild(pestItem);
     });
+}
 
-    weedsButton.addEventListener('click', function () {
-        showWeeds(weedsData);
+function showWeeds(pests) {
+    commonPestList.innerHTML = ''; // Clear the existing content
 
-        weedsButton.style.backgroundColor = '#4CAF50';
-        weedsButton.style.color = '#fff';
+    pests.forEach(pest => {
+        const pestItem = document.createElement('div');
+        pestItem.className = 'pest';
 
-        pestsButton.style.backgroundColor = '#fff';
-        pestsButton.style.color = '#000';
+        const pestLink = document.createElement('a');  // anchor element
+        pestLink.href = pest.destination; // destination page url
 
+        const pestImage = document.createElement('img');
+        pestImage.src = pest.image;
+        pestImage.className = 'listIcon';
 
+        const pestName = document.createElement('p');
+        pestName.textContent = pest.name;
+
+        pestLink.appendChild(pestImage);
+        pestLink.appendChild(pestName);
+        
+        pestItem.appendChild(pestLink);
+        commonPestList.appendChild(pestItem);
     });
-
-    function showPests(pests) {
-        commonPestList.innerHTML = ''; // Clear the existing content
-
-        pests.forEach(pest => {
-            const pestItem = document.createElement('div');
-            pestItem.className = 'pest';
-
-            const pestLink = document.createElement('a');  // anchor element
-            pestLink.href = pest.destination;
-
-            const pestImage = document.createElement('img');
-            pestImage.src = pest.image;
-            pestImage.className = 'listIcon';
-
-            const pestName = document.createElement('p');
-            pestName.textContent = pest.name;
-
-            pestLink.appendChild(pestImage);
-            pestLink.appendChild(pestName);
-
-            pestItem.appendChild(pestLink);
-            commonPestList.appendChild(pestItem);
-        });
-    }
-
-    function showWeeds(pests) {
-        commonPestList.innerHTML = ''; // Clear the existing content
-
-        pests.forEach(pest => {
-            const pestItem = document.createElement('div');
-            pestItem.className = 'pest';
-
-            const pestLink = document.createElement('a');  // anchor element
-            pestLink.href = pest.destination; // destination page url
-
-            const pestImage = document.createElement('img');
-            pestImage.src = pest.image;
-            pestImage.className = 'listIcon';
-
-            const pestName = document.createElement('p');
-            pestName.textContent = pest.name;
-
-            pestLink.appendChild(pestImage);
-            pestLink.appendChild(pestName);
-
-            pestItem.appendChild(pestLink);
-            commonPestList.appendChild(pestItem);
-        });
-    }
+}
 
 
 });
---
+++ searchEntry.js
@@ -0,0 +1,16 @@
+
+class SearchEntry {
+    constructor(
+        name,
+        image
+    ) {
+        this.name = name;
+        this.image = image;
+        this.entry = `
+            <div class = "searchItem">
+                <p class= "searchText">${this.name}</p>
+                <img src="${this.image}" class= "searchItemIcon">
+            </div>
+            `;
+    }
+}
